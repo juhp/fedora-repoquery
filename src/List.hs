cb215a237e5d114dceedf623e78b2833c29c10d8
Jens Petersen <juhpetersen@gmail.com>
2025-09-22 19:56:57 +0800

    List: improve sorting of releases

--- src/List.hs
+++ src/List.hs
@@ -3,13 +3,17 @@ module List (
   ) where
 
 import Data.List.Extra
+import Data.Ord (Down(Down))
 import SimpleCmd ((+-+))
 
 import Release (activeFedoraReleases, BodhiRelease(..))
 
 listVersionsCmd :: IO ()
 listVersionsCmd =
-  activeFedoraReleases >>= mapM_ printRelease . sortOn releaseState
+  activeFedoraReleases >>= mapM_ printRelease . sortOn releaseSorter
   where
     printRelease (Release _version state branch _composed _postbeta) =
       putStrLn $ branch +-+ state
+
+    releaseSorter rel =
+      (Down (releaseState rel), Down(releaseVersion rel))
